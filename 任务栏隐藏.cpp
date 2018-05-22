a0e9b9b91f38f92d16afcd45441c88ffd0954781
KeBugCheckEx <33929772+KiDisplayBlueScreen@users.noreply.github.com>
2018-05-22 16:07:39 +0800

    Rename TaskbarHide.cpp to 任务栏隐藏.cpp

---
+++ 任务栏隐藏.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include <Windows.h>
+
+int main()
+{
+	LPCSTR WndClass = "Shell_TrayWnd";
+	LPCSTR Program = "Progman";
+	HWND TaskBar = FindWindowA(WndClass, 0);
+	HWND DesktopIcon = FindWindowA(Program, 0);
+
+
+	ShowWindow(TaskBar, SW_HIDE);
+	ShowWindow(DesktopIcon, SW_HIDE);
+
+	ShowWindow(TaskBar, SW_NORMAL);
+	ShowWindow(DesktopIcon, SW_NORMAL);
+
+
+
+	PAUSE;
+    return 0;
+}
+
